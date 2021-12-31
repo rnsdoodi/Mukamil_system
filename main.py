de73a04781ec2b4cecee9ce0ed38b04447c60212
rnsdo <rnsdoodi9@gmail.com>
2021-12-31 19:37:41 +0300

    Change sqllite to postgrate Sql

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ all_arrived = []
 
 # Creating The SQLALCHEMY DataBase
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///users.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
