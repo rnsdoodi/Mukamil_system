b6bf4aa5b8b02d86100ebe37281ee86a6980fd70
rnsdo <rnsdoodi9@gmail.com>
2022-08-06 19:44:24 +0300

    adding the user's manual to the home page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
@@ -584,6 +584,11 @@ def conditions():
 @app.route("/privacy")
 def privacy():
     return render_template("privacy.html")
+
+
+@app.route("/download")
+def download():
+    return send_from_directory('static', filename="files/user's manual.pdf")
 
 
 #######################################################################################################################
